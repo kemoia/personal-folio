4a43ae2312d7740b609132c3adc55e94b9962c64
kemoia <kemoiareynolds04@gmail.com>
2022-07-27 06:55:05 -0500

    adding things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import about from './about';
+import home from './home';
+import contact from './contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+    <about/>
+    <contact/>
+    <home/>
     </div>
   );
 }
